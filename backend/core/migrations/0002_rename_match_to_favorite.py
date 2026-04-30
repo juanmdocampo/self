ad1dd013d9ebfcf3611fddc8e9b417f264166bda
juanmdocampo <docampojuan@gmail.com>
2026-04-30 12:37:41 -0300

    fix

--- backend/core/migrations/0002_rename_match_to_favorite.py
+++ backend/core/migrations/0002_rename_match_to_favorite.py
@@ -1,8 +1,6 @@
 # Generated migration to rename Match model to Favorite
 
-import django.db.models.deletion
-from django.conf import settings
-from django.db import migrations, models
+from django.db import migrations
 
 
 class Migration(migrations.Migration):
@@ -15,15 +13,5 @@ class Migration(migrations.Migration):
         migrations.RenameModel(
             old_name='Match',
             new_name='Favorite',
-        ),
-        migrations.AlterField(
-            model_name='user',
-            name='favorites_as_patient',
-            field=models.ManyToOneRel(auto_created=True, field_name='patient', limit_choices_to=None, on_delete=django.db.models.deletion.CASCADE, related_name='favorites_as_patient', to='core.favorite'),
-        ),
-        migrations.AlterField(
-            model_name='user',
-            name='favorites_as_psychologist',
-            field=models.ManyToOneRel(auto_created=True, field_name='psychologist', limit_choices_to=None, on_delete=django.db.models.deletion.CASCADE, related_name='favorites_as_psychologist', to='core.favorite'),
         ),
     ]
